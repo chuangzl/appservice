cdb6859ec5b8c30cea27470f5c4e1c3cd2ee50d5
chuangzl <chuangzl@192.168.31.151>
2017-05-17 18:17:43 +0800

    update

---
+++ appservice/service/proxy.py
@@ -0,0 +1,36 @@
+# -*- coding:utf-8 -*-
+'''
+Created on 2017年5月17日
+
+@author: chuangzl
+'''
+
+import web
+import json
+import uuid
+import sys
+import requests
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+urls = (
+    '/proxy', 'proxy'
+)
+
+app = web.application(urls, globals())
+
+class test:
+    def GET(selfs):
+        #web.header('content-type','text/json')
+        get = web.ctx.get
+        status = web.ctx.status
+        print get
+        print status
+
+        #return_str = str(endpoint_json)
+        #return_str = return_str.replace("'", "\"");
+        return "ok"
+    
+if __name__ == '__main__':
+    app.run()
